695444e0d952b3901901337023a2c7038ef66d8b
Zhang.Cong <1084823834@qq.com>
2017-09-03 23:18:09 +0800

    show the picture

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,4 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  /*@import "~common/stylus/variable";
-
-  #app
-    color: $color-theme*/
 </style>
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,11 +1,11 @@
-import originJsonp from 'jsonp'
+import originJSONP from 'jsonp'
 
+// url为服务端地址，data为拼接到url上的查询参数，option为原jsonp方法所需参数
 export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-
   return new Promise((resolve, reject) => {
-    originJsonp(url, option, (err, data) => {
-      if (!err) {
+    originJSONP(url,option,(err,data) => {
+      if(!err){
         resolve(data)
       } else {
         reject(err)
@@ -14,11 +14,12 @@ export default function jsonp(url, data, option) {
   })
 }
 
-export function param(data) {
+function param(data) {
   let url = ''
   for (var k in data) {
+    // 如果值为undefined传入空否则传入值
     let value = data[k] !== undefined ? data[k] : ''
-    url += '&' + k + '=' + encodeURIComponent(value)
+    url += '&${k}=${encodeURIComponent(value)}'
   }
   return url ? url.substring(1) : ''
 }
