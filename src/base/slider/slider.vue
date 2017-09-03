9be5cfb6a1f5ab08fc52d7d84aa466d77a61cd5f
Zhang.Cong <1084823834@qq.com>
2017-09-03 23:21:29 +0800

    modified the slider.vue

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -37,16 +37,16 @@ export default {
   },
   methods: {
     _setSliderWidth() {
-      // this.children = this.$refs.sliderGroup.children
+      this.children = this.$refs.sliderGroup.children
       //
-      // let width = 0
-      // let sliderWidth = this.$refs.slider.clientWidth
-      // for(let i=0; i<this.children.length; i++) {
-        // let child = this.children[i]
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for(let i=0; i<this.children.length; i++) {
+        let child = this.children[i]
         // addClass(child, 'slider-item')
         // child.style.width = sliderWidth + 'px'
         // width += sliderWidth
-      // }
+      }
       //
       // if(this.loop) {
       //   width += 2 * sliderWidth
