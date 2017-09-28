0e86872b7b9a1e5d03bccd1d132541900117a9f4
Zhang.Cong <1084823834@qq.com>
2017-09-28 21:34:34 +0800

    finish part of the shortcut

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -21,6 +21,10 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
@@ -34,9 +38,16 @@ export default {
         return
       }
       this.scroll = new BScroll(this.$refs.wrapper, {
-        probeType: this.robeType,
+        probeType: this.probeType,
         click: this.click,
       })
+      
+      if (this.listenScroll) {
+        let me = this
+        this.scroll.on('scroll', (pos) => {
+            me.$emit('scroll', pos)
+          })
+      }
     },
     enable() {
       this.scroll && this.scroll.enable()
@@ -47,6 +58,12 @@ export default {
     },
     refresh() {
       this.scroll && this.scroll.refresh()
+    },
+    scrollTo() {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   },
   watch: {
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,97 +1,103 @@
-<template lang="html">
-  <div>
-    <list-view :data="singers"></list-view>
+<template>
+  <div class="singer" ref="singer">
+    <list-view :data="singers" ref="list"></list-view>
   </div>
-
 </template>
 
-<script type="text/ecmascript-6">
-import {
-  getSingerList
-} from 'api/singer'
-import {
-  ERR_OK
-} from 'api/config'
-import Singer from 'common/js/singer'
-import ListView from 'base/listview/listview'
+<script>
+  import ListView from 'base/listview/listview'
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
 
-const HOTE_NAME = "热门"
-const HOTE_SINGERS_LEN = 10
 
-export default {
-  data() {
-    return {
-      singers: []
-    }
-  },
-  created() {
-    this._getSingerList()
-  },
-  methods: {
-    _getSingerList() {
-      getSingerList().then((res) => {
-        if (res.code == ERR_OK) {
-          this.singers = this._normalizeSinger(res.data.list)
-        }
-      })
-    },
-    _normalizeSinger(list) {
-      let map = {
-        hot: {
-          title: HOTE_NAME,
-          items: []
-        }
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
+
+  export default {
+    data() {
+      return {
+        singers: []
       }
-      list.forEach((item, index) => {
-        if (index < HOTE_SINGERS_LEN) {
-          map.hot.items.push(new Singer({
-            id: item.Fsinger_mid,
-            name: item.Fsinger_name,
-          }))
-        }
-        const key = item.Findex
-        if (!map[key]) {
-          map[key] = {
-            title: key,
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.singer.style.bottom = bottom
+        this.$refs.list.refresh()
+      },
+      selectSinger(singer) {
+        this.$router.push({
+          path: `/singer/${singer.id}`
+        })
+        this.setSinger(singer)
+      },
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
             items: []
           }
         }
-        map[key].items.push(new Singer({
-          id: item.Fsinger_mid,
-          name: item.Fsinger_name
-        }))
-      })
-      // console.log("map",map)
-      let hot = []
-      let ret = []
-      for (let key in map) {
-        let val = map[key]
-        if (val.title.match(/[a-zA-Z]/)) {
-          ret.push(val)
-        } else {
-          hot.push(val)
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        console.log(map)
+        // 为了得到有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
         }
+        console.log(hot)
+        console.log(ret)
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
-      // console.log("ret", ret)
-      ret.sort((a, b) => {
-        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
-      })
-
-      return hot.concat(ret)
+    },
+    components: {
+      ListView
     }
-  },
-  components: {
-    ListView
   }
-}
+
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-</style>
-
-<style scoped lang="stylus" rel="stylesheet/stylus">
   .singer
-    postion: fixed
+    position: fixed
     top: 88px
     bottom: 0
     width: 100%
